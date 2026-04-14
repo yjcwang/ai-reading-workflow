1fb6408118c9bd14efff3b4f2c26efa7df9174f0
ge84qoh <ge84qoh@mytum.de>
2026-04-14 14:15:52 +0900

    CORS configuration for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,10 +14,11 @@ def on_startup() -> None:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-    ],
+    ###allow_origins=[
+    ##    "http://localhost:3000",
+    ##    "http://127.0.0.1:3000",
+    ###],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
